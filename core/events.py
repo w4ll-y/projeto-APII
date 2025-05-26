5ac83da3d4b85ed6954f81e0e3b76642f0ffd5e8
W4ll-y <wallacyj.que.j@gmail.com>
2025-05-25 21:29:18 -0300

    feat: full screen mode (temporary solution)

--- core/events.py
+++ core/events.py
@@ -1,10 +1,15 @@
 import pygame
 import sys
+from settings import WIDTH, HEIGTH
 from levels.level import Level
 
 class Events:
     def __init__(self, game):
         self.game = game
+
+        self.full_screen = False
+        self.window_w = WIDTH
+        self.window_h = HEIGTH
 
     def run(self):
         for event in pygame.event.get():
@@ -15,5 +20,24 @@ class Events:
                 width, heigth = event.w, event.h
 
                 self.game.screen = pygame.display.set_mode((width, heigth), pygame.RESIZABLE)
+
+                if self.full_screen:
+                    self.game.screen = pygame.display.set_mode((self.window_w, self.window_h), pygame.RESIZABLE)
+                    self.full_screen = False
+
                 self.game.level.visible_sprites.save_window_size()
+
+
+            if event.type == pygame.KEYDOWN:
+                match event.key:
+                    case pygame.K_F11:
+                        if not self.full_screen:
+                            self.window_w, self.window_h = self.game.screen.get_size()
+
+                            self.game.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+                            self.full_screen = True
+                        else:
+                            self.game.screen = pygame.display.set_mode((self.window_w, self.window_h))
+                        
+                        self.game.level.visible_sprites.save_window_size()
 
