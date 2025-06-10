f611679574dbd36459b2d12fff91a89aadad5dd2
W4ll-y <wallacyj.que.j@gmail.com>
2025-06-10 12:31:01 -0300

    feat: create interactive chests

--- levels/tile.py
+++ levels/tile.py
@@ -3,12 +3,14 @@ from utils.enums import OpenMapTileType
 from settings import TILESIZE, ZOOM
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos: tuple, original_pos: tuple, groups: list, sprite_type: str, surface = pygame.Surface((TILESIZE * ZOOM, TILESIZE * ZOOM))):
+    def __init__(self, pos: tuple, original_pos: tuple, original_value: int, groups: list, sprite_type: str, surface = pygame.Surface((TILESIZE * ZOOM, TILESIZE * ZOOM))):
         super().__init__(groups)
 
         self.sprite_type = sprite_type
         self.image = surface
+        
         self.original_pos = original_pos
+        self.original_value = original_value
 
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0, -5)
