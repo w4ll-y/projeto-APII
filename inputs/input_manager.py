f2d34175109f15dac243213c1f076aab9b789377
w4ll-y <wallacyj.que.j@gmail.com>
2025-08-07 11:13:20 -0300

    feat: joystick its working

--- inputs/input_manager.py
+++ inputs/input_manager.py
@@ -7,6 +7,12 @@ from utils.enums import InputType
 class InputManager():
     def __init__(self):
         self.input_type = InputType.JOYSTICK
+
+        try:
+            self.joystick = pygame.joystick.Joystick(0)
+            self.joystick.init()
+        except pygame.error:
+            self.joystick = None
     
     def set_input_type(self, events):
         for event in events:
@@ -16,13 +22,7 @@ class InputManager():
                 self.input_type = InputType.JOYSTICK
 
     def get_input(self) -> InputInterface:
-        if self.input_type == InputType.JOYSTICK:
-            try:
-                joystick = pygame.joystick.Joystick(0)
-                joystick.init()
-
-                return Joystick(joystick)
-            except pygame.error:
-                return Keyboard()
+        if self.input_type == InputType.JOYSTICK and self.joystick != None:
+            return Joystick(self.joystick)
         else:
             return Keyboard()
