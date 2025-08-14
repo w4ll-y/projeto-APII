0d2dca2bdbe92f5386f877229549504e46a2e488
w4ll-y <wallacyj.que.j@gmail.com>
2025-08-14 10:29:32 -0300

    feat: reset save when the game is closed

--- core/event.py
+++ core/event.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from settings import WIDTH, HEIGTH
+from utils.suport import reset_game
 
 class Event:
     def __init__(self, game):
@@ -15,6 +16,7 @@ class Event:
 
         for event in events:
             if event.type == pygame.QUIT:
+                reset_game()
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.VIDEORESIZE:
