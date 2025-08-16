44ee4336a2ecb007cd5ce8b0361025054393a9f0
0Erik1 <erikpinheiroalves@gmail.com>
2025-08-16 12:48:05 -0300

    add guns and knife

--- settings.py
+++ settings.py
@@ -23,8 +23,13 @@ ENERGY_BORDER_COLOR = '#4b726e'
 ENERGY_COLOR = '#8caba1'
 
 WEAPON_DATA = {
-    0: {'name': 'facao', 'cooldown': 100 , 'damage': 15, 'graphic': 'assets/sprites/weapons/facao/right.png', 'energy_spent': 0},
-    1: {'name': 'espingarda', 'cooldown': 100 , 'damage': 30, 'graphic': 'assets/sprites/weapons/espingarda/right.png', 'energy_spent': DEFAULT_ENERGY_STATS_VALUE * 2}
+    0: {'name': 'faca', 'cooldown': 100 , 'damage': 15, 'graphic': 'assets/sprites/weapons/faca/right.png', 'energy_spent': 0},
+    1: {'name': 'facao', 'cooldown': 100 , 'damage': 30, 'graphic': 'assets/sprites/weapons/facao/right.png', 'energy_spent': 0}
+}
+
+GUNS_DATA = {
+    0:{'name': 'espingarda', 'cost': 5, 'damage': 20, 'speed': 20, 'max_range': 200, 'cooldown': 100 , 'graphic': 'assets/sprites/weapons/espingarda/right.png' },
+    1:{'name': 'revolver', 'cost': 5, 'damage': 20, 'speed': 20, 'max_range': 500, 'cooldown': 100 , 'graphic': 'assets/sprites/weapons/revolver/right.png' }
 }
 
 ENEMY_DATA = {
