b176ec38a738d5b4d87338375a7b3cb943c13ba8
w4ll-y <wallacyj.que.j@gmail.com>
2025-08-16 15:57:58 -0300

    fix: return with some values in settings

--- settings.py
+++ settings.py
@@ -4,6 +4,9 @@ FPS = 60
 TILESIZE = 32
 ZOOM = 1.5
 
+DEFAULT_STATS_VALUE = 50 #The player's default stats must be a multiple of 50
+DEFAULT_ACTUAL_STATS_VALUE = 25 #The actual player stats must be a multiple of 25
+
 HEALTH_HEIGHT = 64
 HEALTH_WIDTH = 64
 
