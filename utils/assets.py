ecf8a353037819976bf90c3ea19ac7915ab39816
w4ll-y <wallacyj.que.j@gmail.com>
2025-08-19 18:07:00 -0300

    feat: added history

--- utils/assets.py
+++ utils/assets.py
@@ -1,18 +1,20 @@
-GAME_HISTORY = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, \n" \
-"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim \n" \
-"ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip \n" \
-"ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate \n" \
-"velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat \n" \
-"cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id \n" \
-"est laborum. *paragraph*" \
-"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium \n" \
-" doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore \n" \
-"veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim \n" \
-"ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia \n" \
-"consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque \n" \
-"porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci \n" \
-"velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore \n" \
-"magnam aliquam quaerat voluptatem."
+GAME_HISTORY = "A vida era dura, mas ainda assim tranquila... até que eles chegaram: *paragraph*" \
+"os Cangaceiros. *paragraph*" \
+"Não um bando qualquer, mas os temidos homens de Lampião. \n" \
+"Todos conheciam sua fama e sabiam da crueldade de sua gangue, mas o que se viu \n" \
+"naquela noite foi além de qualquer pesadelo: corpos espalhados pelas ruas, \n" \
+"casas reduzidas a cinzas, e um Lampião fora de si... a ponto de sequestrar \n" \
+"o próprio Padre Cícero. *paragraph*" \
+"A brutalidade era tanta que até seus comparsas começaram a duvidar da sanidade \n" \
+"do líder. Entre eles, Candeeiro, parceiro de uma vida inteira. *paragraph*" \
+"Quando ousou confrontá-lo, Lampião não hesitou: mirou no peito do amigo. *paragraph*" \
+"Um único disparo ecoou. O corpo de Candeeiro tombou no chão, sangrando, \n" \
+"abandonado por seu bando e traído pelo irmão de armas. *paragraph*" \
+"Por milagre, um vendedor local o encontrou. Escondido em um abrigo, \n" \
+"foi tratado por moradores que ainda resistiam à tirania. *paragraph*" \
+"Agora, renascido da morte, Candeeiro tem apenas um propósito: deter Lampião... \n" \
+"e salvar Padre Cícero. *paragraph*" \
+"O mais rápido possível."
 
 NPC_1_DIALOG = "Os cangaceiros são muito perigosos, ouvi falar \n" \
 "que eles têm bases escondidas para guardar tesouros e armas! *paragraph*\n" \
