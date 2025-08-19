35e6a151d726106b57398b6b3152849432a0dc5e
w4ll-y <wallacyj.que.j@gmail.com>
2025-08-19 14:02:46 -0300

    feat: added game title in the main menu

--- ui/menu/main_menu.py
+++ ui/menu/main_menu.py
@@ -41,6 +41,11 @@ class MainMenu():
 
         self.display_surface.blit(overlay, (0, 0))
 
+        game_title_graphic = resize_image(f'assets/graphics/hud/game_title/title.png', 2.5)
+        game_title_rect = game_title_graphic.get_rect(midtop= (self.display_surface.get_width() // 2, 60))
+
+        self.display_surface.blit(game_title_graphic, game_title_rect)
+
         #display best time
         if len(self.best_times) > 0:
             title_font = pygame.font.Font(size=24)
